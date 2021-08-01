503c75763ffd2f0c97588048f0774df2bdddaf22
Aiden Isaia <aidenisaia@gmail.com>
2021-08-01 15:50:17 -0700

    login, logout, and signup actions

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
+import Logout from "../views/Logout.vue";
+import PostsIndex from '../views/PostsIndex.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +13,26 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  { 
+    path: "/signup", 
+    name: "signup", 
+    component: Signup 
+  },
+  { 
+    path: "/login", 
+    name: "login", 
+    component: Login 
+  },
+  { 
+    path: "/logout", 
+    name: "logout", 
+    component: Logout 
+  },
+  {
+    path: '/posts',
+    name: 'posts-index',
+    component: PostsIndex
   },
   {
     path: '/about',
---
+++ src/views/PostsIndex.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    {{ posts }}
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      posts: [],
+    };
+  },
+  created: function () {
+    this.indexPosts();
+  },
+  methods: {
+    indexPosts: function () {
+      axios.get("/posts").then((response) => {
+        console.log(response.data);
+        this.posts = response.data;
+      });
+    },
+  },
+};
+</script>
