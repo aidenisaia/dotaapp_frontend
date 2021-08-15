adfaef9fe5ea1c8e8e018b1dfb2aff13f766e646
Aiden Isaia <aidenisaia@gmail.com>
2021-08-15 13:36:57 -0700

    post, post comments, post user, and post comments user, all showing up at post index

--- src/views/PostsIndex.vue
+++ src/views/PostsIndex.vue
@@ -1,10 +1,30 @@
 <template>
   <div class="home">
-    {{ posts }}
+    <div v-for="post in posts">
+      <h2>{{ post.title }}</h2>
+      <h3>{{ users.find(x => x.id === post.user_id).username }}</h3>
+      <p>{{ post.body }}</p>
+      <br>
+      <div v-for="comment in comments">
+        <p v-if="comment.post_id === post.id">
+          {{ comment.body }}
+          <br>
+          - {{ users.find(x => x.id === comment.user_id).username }}
+        </p>
+      </div>
+      <hr>
+    </div>
   </div>
 </template>
 
-<style></style>
+<style>
+h3 {
+  margin-top: 0;
+}
+h2 {
+  margin-bottom: 0;
+}
+</style>
 
 <script>
 import axios from "axios";
@@ -13,16 +33,32 @@ export default {
   data: function () {
     return {
       posts: [],
+      comments: [],
+      users: [],
     };
   },
   created: function () {
     this.indexPosts();
+    this.indexComments();
+    this.indexUsers();
   },
   methods: {
     indexPosts: function () {
       axios.get("/posts").then((response) => {
         console.log(response.data);
         this.posts = response.data;
+      });
+    },
+    indexComments: function () {
+      axios.get("/comments").then((response) => {
+        console.log(response.data);
+        this.comments = response.data;
+      });
+    },
+    indexUsers: function () {
+      axios.get("/users").then((response) => {
+        console.log(response.data);
+        this.users = response.data;
       });
     },
   },
